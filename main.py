8da96be2efba9ab108fc10bbc3098b741ed75066
Juan Diego Pineda Calvo <juanpincal@unisabana.edu.co>
2026-04-13 22:44:45 -0500

    v1 password_manager.py

--- main.py
+++ main.py
@@ -3,7 +3,48 @@ from password_manager import add_login, change_password, encrypt_passwords_in_fi
 
 def main() -> None:
     """TODO: Parte 5 - programa principal interactivo."""
-    pass
+    
+    filename = input("Enter the CSV file name:/n").strip()
+
+    encrypt_passwords_in_file(filename)
+
+    while True:
+        option = input("options: (1) Change Password, (2) Add Password, (3) Quit:/n").strip()
+
+        if option == "1":
+            entry = input("Enter the website and the new password:\n").strip().split()
+            if len(entry) < 2:
+                print("Input is in the wrong format!")
+                continue
+
+            website, new_password = entry[0], entry[1]
+            if len(new_password) < 12:
+                print("Password is too short!")
+                continue
+
+            success = change_password(filename, website, new_password)
+            if success:
+                print("Password changed.")
+            else:
+                print("Website not found! Operation failed.")
+
+        elif option == "2":
+
+            entry = input("Enter the website, username, and password:\n").strip().split()
+            if len(entry) < 3:
+                print("Input is in the wrong format!")
+                continue
+            
+            website, username, password = entry[0], entry[1], entry[2]
+            if len(password) < 12:
+                print("Password is too short!")
+                continue
+            add_login(filename, website, username, password)
+        
+        elif option == "3":
+            break
+        else:
+            print("Invalid option! selected!")
 
 
 if __name__ == "__main__":
--- password_manager.py
+++ password_manager.py
@@ -4,20 +4,68 @@ from caesar import caesar_encrypt
 
 
 def encrypt_single_pass(filename: str) -> None:
-    """TODO: Parte 1."""
-    pass
+    """
+    Lee una contraseña desde un archivo de texto, la encripta con caesar_encrypt
+    y sobrescribe el archivo con la versión encriptada.
+    """
+    with open(filename, "r") as f:
+        password = f.rad().strip()
 
+    encrypted = caesar_encrypt(password)
+
+    with open(filename, "w") as f:
+        f.write(encrypted)
 
 def encrypt_passwords_in_file(filename: str) -> None:
-    """TODO: Parte 2."""
-    pass
+    """
+    Lee un archivo CSV, encripta únicamente la columna de contraseñas,
+    y sobrescribe el archivo con los datos actualizados.
+    """
+    rows = []
+    with open(filename, "r", newline="") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row: #evitar filas vacías
+                # No encriptar la cabecera
+                if row[0].lower() != "website":
+                    row[2] = caesar_encrypt(row[2].strip())
+                rows.append(row)
+
+    with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
+    
 
 
 def change_password(filename: str, website: str, password: str) -> bool:
-    """TODO: Parte 3."""
-    pass
+    """
+    Cambia la contraseña de un sitio web en el archivo CSV.
+    Retorna True si el sitio fue encontrado y actualizado, False en caso contrario.
+    """
+    Updated = False
+    rows = []
+    with open(filename, "r", newline="") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row:
+                if row[0].lower() == website.lower():
+                    row[2] = caesar_encrypt(password.strip())
+                    updated = True
+                rows.append(row)
+            
+
+    if updated:
+        with open(filename, "w", newlines="") as f:
+            writer = csv.writer(f)
+            writer.writerrows(rows)
+    return updated
 
 
 def add_login(filename: str, website_name: str, username: str, password: str) -> None:
-    """TODO: Parte 4."""
-    pass
+    """
+    Agrega una nueva entrada de login al archivo CSV.
+    """
+    encrypted = caesar_encrypt(password.strip())
+    with open(filename, "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([website_name, username, encrypted])
